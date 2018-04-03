b986fbe706661e8afe10d3d671febe203ea9ddc4
elimoss <elimoss@stanford.edu>
2018-04-03 15:20:20 -0700

    Reformatted to operate with slurm scg profile, other debugging.

--- snakemake_submit.sh
+++ snakemake_submit.sh
@@ -1,10 +1,7 @@
-snakemake \
-  --jobs 100 \
-  --configfile /srv/gsfs0/projects/bhatt/ribado/tools/qc_snakemake/config.json \
-  --cluster "sbatch --account=asbhatt \
-                    	  --job-name {params.job_name} \
-                        -t {params.max_time} \
-                        --mem={params.max_mem} \
-                        -n {threads} \
-                        -o {params.log_files}.log \
-                        -e {params.log_files}.error"
+snakemake -np \
+--jobs 100 \
+--configfile config.yaml \
+--cluster-config clusterconfig.yaml \
+--restart-times 0 \
+-s Snakefile \
+-profile scg
